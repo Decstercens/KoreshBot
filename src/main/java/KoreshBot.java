329df13d973ad4cefdfd63add1b613393fca1d0a
Сагинбаев Артур <decsterce@gmail.com>
2022-08-12 16:45:40 +0500

    First implementation of KoreshBot. Creation of bot and added reaction to text event.

--- src/main/java/KoreshBot.java
+++ src/main/java/KoreshBot.java
@@ -1,5 +1,29 @@
-public class KoreshBot {
-    public static void main(String[] args) {
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import javax.security.auth.login.LoginException;
+
+public class KoreshBot extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        String token="";
+        JDABuilder.createLight(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.MESSAGE_CONTENT)
+                .addEventListeners(new KoreshBot())
+                .setActivity(Activity.playing("Type !ping"))
+                .build();
+    }
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        System.out.println("We received a message from "+
+                event.getAuthor().getName()+
+                " he wrote: "+
+                event.getMessage().getContentRaw());
+        if (event.getMessage().getContentRaw().equals("!ping"))
+        {
+            event.getChannel().sendMessage("Pong!").queue();
+        }
     }
 }
